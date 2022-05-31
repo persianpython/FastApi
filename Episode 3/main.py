f8b8d76ecda3fc86f732af4a257974ec86d8638d
Persian Python <persianpython2000@gmail.com>
2022-06-01 01:49:53 +0430

    قسمت سوم - Query Parameter

---
+++ Episode 3/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from enum import Enum
+from typing import Optional
+
+app = FastAPI()
+
+@app.get('/')
+def hello():
+    return 'hello world'
+
+
+class TypeBlogs(str, Enum):
+    Mesal1 = 'mesal1'
+    Mesal2 = 'mesal2'
+    Mesal3 = 'mesal3'
+
+@app.get('/blog/{id}/comments/{comment_id}')
+def get_comment(id:int, comment_id:int, valid:bool=True, username:Optional[str]=None):
+    return {"message":f"blog id {id} comment id {comment_id} {valid=} {username=}"}
+
+
+@app.get('/blog/all')
+def get_blogs(page:Optional[int]=None, page_size:str=None):
+    return {"message": f"{page=} -- {page_size=}"}
+
+
+@app.get('/blog/type/{type}')
+def get_type_blog(type:TypeBlogs):
+    return {'message': f'blog type is {type}'}
+
+# @app.get('/blog/all')
+# def get_blogs():
+#     return {'message':f'all blogs'}
+
+@app.get('/blog/{id}')
+def get_blog(id:int):
+    return {'message':f'blog {id}'}
+
